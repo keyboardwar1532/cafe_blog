0019547f814cf657897194662e660e6df3bb4036
Rails Cafe <cafe153220@gmail.com>
2019-07-07 19:22:42 +0800

    Add admin User functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,7 @@ class ArticlesController < ApplicationController
 
   def create
   	@article = Article.new(article_params)
-    @article.user=User.first
+    @article.user=current_user
   	if @article.save
   		flash[:success]="Article was succesfully created"
   		redirect_to article_path(@article)
@@ -57,7 +57,7 @@ class ArticlesController < ApplicationController
     end
 
     def require_same_user
-      if current_user != @article.user
+      if current_user != @article.user and !current_user.admin?
         flash[:danger] = "You can only edit or delete your own article"
         redirect_to root_path
       end
